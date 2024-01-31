c4ca98be3d838abaeef8255c25fa244e6751d556
LorenzoFabbro21 <fabbro.lorenzo@edu.unito.it>
2024-01-31 17:57:30 +0100

    service discovery server

---
+++ service-discovery-server/src/main/java/microservice/servicediscoveryserver/ServiceDiscoveryServerApplication.java
@@ -0,0 +1,14 @@
+package microservice.servicediscoveryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class ServiceDiscoveryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceDiscoveryServerApplication.class, args);
+	}
+
+}
