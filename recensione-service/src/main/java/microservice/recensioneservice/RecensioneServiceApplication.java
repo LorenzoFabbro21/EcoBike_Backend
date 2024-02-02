e8b9bae800bf293f78ef5a9615aa90b364e4ecca
LorenzoFabbro21 <fabbro.lorenzo@edu.unito.it>
2024-02-02 17:47:49 +0100

    add recensione service

---
+++ recensione-service/src/main/java/microservice/recensioneservice/RecensioneServiceApplication.java
@@ -0,0 +1,13 @@
+package microservice.recensioneservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecensioneServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecensioneServiceApplication.class, args);
+	}
+
+}
