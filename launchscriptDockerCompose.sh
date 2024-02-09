5aab814d3ae857fbadec567b349a9a9eb0739713
LorenzoFabbro21 <fabbro.lorenzo@edu.unito.it>
2024-02-09 14:34:28 +0100

    wip
    versione non funzionante ma con alcune modifiche nei dockerfile

--- launchscriptDockerCompose.sh
+++ launchscriptDockerCompose.sh
@@ -1,20 +1,20 @@
 cd api-gateway
-./mvnw  clean package
+./mvnw -DskipTests=true clean package
 docker build -t api-gateway .
 
 cd ../user-service
-./mvnw  clean package
+./mvnw -DskipTests=true clean package
 docker build -t user-service .
 
 cd ../negozio-service
-./mvnw  clean package
+./mvnw -DskipTests=true clean package
 docker build -t negozio-service .
 
 cd ../recensione-service
-./mvnw  clean package
+./mvnw -DskipTests=true clean package
 docker build -t recensione-service .
 
 cd ../service-discovery-server
-./mvnw  clean package
+./mvnw -DskipTests=true clean package
 docker build -t service-discovery-server .
 cd ..
